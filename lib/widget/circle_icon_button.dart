be07acd5fb8f7b5eb34d06ea7b62a9044e91fcb2
mahsudbek1994 <mikki.12022021@gmail.com>
2022-05-21 17:03:53 +0500

    padding 10->5

--- lib/widget/circle_icon_button.dart
+++ lib/widget/circle_icon_button.dart
@@ -12,7 +12,7 @@ class CircleIconButton extends StatelessWidget {
     return Container(
       width: 20,
       height: 20,
-      padding: EdgeInsets.all(10),
+      padding: EdgeInsets.all(5),
       decoration: BoxDecoration(color: color, shape: BoxShape.circle),
       child: SvgPicture.asset(iconUrl),
     );
